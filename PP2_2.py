b110b2fca94002ab6a8389cbbde20a4ec84c07e8
Owais-Khan-ICD2O1-01 <owaisali.khan@student.tdsb.on.ca>
2024-10-15 14:24:34 +0000

    answer

--- PP2_2.py
+++ PP2_2.py
@@ -1,25 +1,46 @@
-
+'''
+    Lesson: If and Else
+    Author: Owais Ali Khan
+    Date Created: October 15, 2024
+    Date Last Modified: October 15, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
+  num = int(input("Input an integer: "))
+  if num == 5:
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 
 
 def q2(): 
-  #Write Assignment code here
+  num = float(input("Input a number: "))
+  if num >= 0:
+    print("Positive")
+  else:
+    print("Negative")
 
 def q3(): 
-  #Write Assignment code here
-
+  num = int(input("Input an integer: "))
+  if num % 2 == 0:
+    print("Even")
+  else:
+    print("Odd")
 def q4(): 
-  #Write Assignment code here
+  string = input('Type "Hello": ')
+  if string == "Hello":
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
+'''
